a41b264f985d0c53a04996a063bf16c399ef0729
yhw320 <123914167+yhw320@users.noreply.github.com>
2023-08-16 10:14:51 +0800

    Update Slurm.sh

--- SchedulerScripts/Slurm.sh
+++ SchedulerScripts/Slurm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=job_name  # Set the job name
-#SBATCH --nodes=1  # Set the required number of nodes
-#SBATCH --ntasks-per-node=4  # Set the required number of CPU cores per node
-#SBATCH --time=01:00:00  # Set the maximum runtime for the job
+#SBATCH --job-name=job_name    # Set the job name
+#SBATCH --nodes=1              # Set the required number of nodes
+#SBATCH --ntasks-per-node=4    # Set the required number of CPU cores per node
+#SBATCH --time=01:00:00        # Set the maximum runtime for the job
 
 # Optional directives:
 #SBATCH --partition=queue_name  # Set the queue name
-#SBATCH --output=output_file  # Set the output file path
-#SBATCH --error=error_file  # Set the error file path
+#SBATCH --output=output_file    # Set the output file path
+#SBATCH --error=error_file      # Set the error file path
 
 # Change to the work_directory
 cd your_path/outputDir1_S17/
